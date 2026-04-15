c66ffb361239d291ba3b9238149b8a61a60e4d00
Xavier Dupré <xadupre@microsoft.com>
2026-04-15 12:18:10 +0200

    une_fonction2

--- test_mon_module.py
+++ test_mon_module.py
@@ -1,10 +1,13 @@
 import unittest
 
-from mon_module import une_fonction
+from mon_module import une_fonction, une_fonction2
 
 class TestMonModule(unittest.TestCase):
     def test_une_function(self):
         self.assertEqual(une_fonction(4 , 5), 9)
+
+    def test_une_function2(self):
+        self.assertEqual(une_fonction2(4 , 5), 9)
 
 if __name__ == "__main__":
     unittest.main()
